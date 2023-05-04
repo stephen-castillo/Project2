918d2811355633f0b2341c9f7e7857b27016b7b3
unknown <sandra.gutierrez.ru@gmail.com>
2023-05-03 19:47:23 -0500

    added login/logout functionality, initial User model, authentication

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-//const userRoutes = require('./userRoutes'); //depends on name given to routes folder(s) in api folder
+const userRoutes = require('./userRoutes'); //depends on name given to routes folder(s) in api folder
 
-//router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,12 @@
 const sequelize = require('../config/connection');
-//const { User } = require('../models');
+const { User } = require('../models');
 
-//const userData = require('./userData.json');
+const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
- await User.bulkCreate(userData, { ////
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
