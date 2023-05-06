4e41dfc2c7b8d76415a89fefd2927898aee0bf64
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-05-06 16:20:30 -0500

    Delete controller directory

--- controller/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const apiRoutes = require('./api');
-const homeRoutes = require('../home_routes.js');
-
-router.use('/', homeRoutes);
-router.use('/routes', apiRoutes);
-
-module.exports = router;
