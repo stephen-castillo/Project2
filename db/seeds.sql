c31afd9310be7441f4da0c8248c9759b8c5e86c5
rnutall <rnutall1@gmail.com>
2023-05-07 03:22:50 -0500

    updated seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -34,9 +34,18 @@ VALUES ("id"),
        ("createdAt"),
        ("updatedAt");
 
-       INSERT INTO user 
+       INSERT INTO user_name 
        (name)
 VALUES ("id"),
        ("username"),
        ("email"),
        ("password");
+
+
+-- put here in case we need it 
+    --    INSERT INTO user (id, quiz, results, active)
+--VALUES (001, "Algebra I", "Linear equations, inequalities, functions, and graphs", true),
+    --    (002, "Pharmacy Tech", "Polynomials, Complex Numbers, Vectors", true),
+    --    (003, "Driving Test", "Limits, Differentiation, Derivatives", true),
+    --    (004, "Koma Sutra", "Intuitively Appealing Axioms Galore", false);
+       
