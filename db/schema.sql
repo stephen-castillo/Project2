878209e3b453053a8796956c44f161cb3fbd7147
rnutall <rnutall1@gmail.com>
2023-05-06 19:11:07 -0500

    updated db

--- db/schema.sql
+++ db/schema.sql
@@ -1,57 +1,6 @@
-CREATE DATABASE quickdecks;
 
--- Create two new databases --
+-- Create one new databases --
 DROP DATABASE IF EXISTS quickdecks;
 CREATE DATABASE quickdecks;
 
--- Use quickdecks --
-USE quickdecks;
 
-CREATE TABLE user
-(
-    first_name VARCHAR(30) NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    phone TEXT NOT NULL,
-    equipment_donated TEXT NOT NULL,
-    date_donated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-CREATE TABLE user_dashboard
-(
-    id INT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    email BOOLEAN NOT NULL,
-    phone TEXT NOT NULL,
-    equipment_donated TEXT NOT NULL,
-    equipment_recieved TEXT NOT NULL,
-    date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-CREATE TABLE inventory
-(
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    email BOOLEAN NOT NULL,
-    phone TEXT NOT NULL,
-    product_name TEXT NOT NULL,
-    equipment_donated TEXT NOT NULL,
-    connection_type TEXT NOT NULL,
-    date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-CREATE TABLE customers
-(
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    email BOOLEAN NOT NULL,
-    phone TEXT NOT NULL,
-    equipment_recieved TEXT NOT NULL,
-    date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-
--- See database in use --
-SELECT DATABASE
-();
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,15 @@
 -- Create a new database called 'quickdecks'
 
 -- Connect to the 'master' database to run this snippet
-USE master
-GO
+--USE master
+--GO
 -- Create the new database if it does not exist already
-IF NOT EXISTS (
-    SELECT name quickdecks
-        FROM sys.databases
-        WHERE name = N'quickdecks'
+--IF NOT EXISTS (
+    --SELECT name quickdecks
+        --FROM sys.databases
+        --WHERE name = N'quickdecks'
         
-)
-CREATE DATABASE quickdecks
+--)
+--CREATE DATABASE quickdecks
 
-GO
+--GO
