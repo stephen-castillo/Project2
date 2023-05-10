7460733d4a618eaf78e9a984c2acbe71b3ed7144
unknown <sandra.gutierrez.ru@gmail.com>
2023-05-09 20:28:36 -0500

    formatting

--- public/js/partials.js
+++ public/js/partials.js
@@ -1,4 +1,4 @@
-// Scripts to register handlebars partials
+// Scripts to register navbar handlebars partial
 
 const Handlebars = require('handlebars');
 const fs = require('fs');
@@ -8,17 +8,3 @@ const fs = require('fs');
 const navbar = fs.readFileSync(__dirname + '/../views/partials/navbar.handlebars', 'utf8');
 Handlebars.registerPartial('navbar', navbar);
 
-// Flashcard page 
-
-const flashcardpage = fs.readFileSync(__dirname + '/../views/partials/flashcardpage.handlebars', 'utf8');
-Handlebars.registerPartial('flashcards', flashcardpage);
-
-// Quiz page
-
-const quizpage = fs.readFileSync(__dirname + '/../views/partials/quizpage.handlebars', 'utf8');
-Handlebars.registerPartial('quiz', quizpage);
-
-// Category page
-
-const categorypage = fs.readFileSync(__dirname + '/../views/partials/category.handlebars', 'utf8');
-Handlebars.registerPartial('category', categorypage);
