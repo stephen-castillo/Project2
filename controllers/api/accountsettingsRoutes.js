df0a4f5c90a4972aafafa27e772514dc9c8baee1
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-05-09 20:19:22 -0500

    fixed accountsettingRoutes error

--- controllers/api/accountsettingsRoutes.js
+++ controllers/api/accountsettingsRoutes.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
 
 router.get('/accountsettings', withAuth, (req, res) => {
     res.render('accountsettings', {
