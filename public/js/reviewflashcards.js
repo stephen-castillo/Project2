55950cf9b4d55be789c221a4dcbc10d2dec1b980
unknown <sandra.gutierrez.ru@gmail.com>
2023-05-10 01:48:41 -0500

    generate card thumbnails js

--- public/js/reviewflashcards.js
+++ public/js/reviewflashcards.js
@@ -35,24 +35,37 @@ lastFlashcard.addEventListener('click', () => {
 });
 
 
-//For dashboard
+
+
+
+//For dashboard, under My Flashcards
+
+const createFlashcardsBtn = document.querySelector('#create-flashcards-btn');
+
+createFlashcardsBtn.addEventListener('click', () => {
+
+
+// Get the form input values
+const title = document.querySelector('#deck-title').value;
+const category = document.querySelector('#deck-category').value;
+
+function generateCard(title, category) {
+  return `
+    <div class="card container bg-dark" style="width: 18rem;">
+      <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-subtitle">Category: ${category}</p>
+      </div>
+    </div>
+  `;
+}
 
 // Create a new card element
 const card = document.createElement('div');
 card.classList.add('card');
 
-card.innerHTML = `
-{{#each flashcards}}
-<div class="card container bg-dark" style="width: 18rem;">
-  <div class="card-body">
-      <h5 class="card-title">{{this.title}}</h5>
-      <p class="card-subtitle">Category: {{this.Category.name}}</p>
-      <a href="/api/flashcards/{{this.id}}" class="card-link">Select</a>
-      <a href="/flashcards/{{this.id}}">Edit</a>
-  </div>
-</div>
-{{/each}}
-`
+// Generate the card HTML with the form input values
+card.innerHTML = generateCard(title, category);
 
 // Create a new link element and set its attributes
 const link = document.createElement('a');
@@ -62,6 +75,8 @@ link.textContent = 'View Flashcards';
 // Append the link element to the card element
 card.appendChild(link);
 
-// Append the card element to the dashboard container
+// Append the card element to the dashboard container and the link element to the card
 const myFlashcards = document.querySelector('#flashcard-thumbnail');
 myFlashcards.appendChild(card);
+
+});
